d0a9b5be4b7b4cb57b37487e06c806ccdcc431ed
JalzAe <jalzae@gmail.com>
2023-10-22 07:17:25 +0700

    nyicil cucumber

---
+++ testing/src/test/java/stepDef/Checkout.java
@@ -0,0 +1,56 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Checkout {
+
+  @Given("access saucedemo then get login to inventory")
+  public void access_saucedemo_then_get_login_to_inventory() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("scrap all inventory item name")
+  public void scrap_all_inventory_item_name() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("add to product on cart")
+  public void add_to_product_on_cart() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("check the number on cart detail")
+  public void check_the_number_on_cart_detail() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("access cart page and get item count")
+  public void access_cart_page_and_get_item_count() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("click checkout button")
+  public void click_checkout_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("fill the form uncorrectly")
+  public void fill_the_form_uncorrectly() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("show error require")
+  public void show_error_require() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+}
---
+++ testing/src/test/java/stepDef/Sorting.java
@@ -0,0 +1,26 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Sorting {
+
+  @Given("access saucedemo then get login to inventory")
+  public void access_saucedemo_then_get_login_to_inventory() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("scrap all inventory item name")
+  public void scrap_all_inventory_item_name() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("change sorting z to a and get desc item by alphabet correctly")
+  public void change_sorting_z_to_a_and_get_desc_item_by_alphabet_correctly() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+}
